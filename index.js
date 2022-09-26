99023e6f9e2f5e0523d15e697c0ec655b9ca2408
AFROS <AFROS@DESKTOP-ETQHKOC>
2022-09-26 13:19:13 +0700

    first

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
+
+app.use(express.json());//
+app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+
+app.get('/', (req,res)=>{
+    res.send('Main page 2');
+});
+
+app.get('/test', (req,res)=>{
+    res.send('Test page 2');
+});
+
+
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+start();
