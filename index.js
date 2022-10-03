75d892b3d2134644fa508af4a054579bcce5ea29
AFROS <AFROS@DESKTOP-ETQHKOC>
2022-10-03 13:39:14 +0700

    dvjbndhv

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 
 app.get('/', (req,res)=>{
-    res.send('Main page 3');
+    res.send('Today is 3 october');
 });
 
 app.get('/test', (req,res)=>{
